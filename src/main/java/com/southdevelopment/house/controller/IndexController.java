858caf14b3c0cae0f2dcb166ef91e46ddeaba1fb
guanyh-bee <543841473@qq.com>
2020-08-05 00:50:37 +0800

    cao

---
+++ src/main/java/com/southdevelopment/house/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.southdevelopment.house.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+
+}
---
+++ src/main/resources/db/migration/V1__create_database_south_development.sql
@@ -0,0 +1,1 @@
+create database south_development character set utf8;
